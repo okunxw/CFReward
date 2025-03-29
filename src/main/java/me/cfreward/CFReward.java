7363bdfd610e5d4746c8f5c11b579d92bcc90a25
okunxw <prigodskiy06@mail.ru>
2025-03-29 15:29:41 +0800

    Update CFReward.java

--- src/main/java/me/cfreward/CFReward.java
+++ src/main/java/me/cfreward/CFReward.java
@@ -1,78 +1,76 @@
 package me.okunxw.cfreward;
 
+import me.okunxw.cfreward.commands.PromoCommand;
+import me.okunxw.cfreward.utils.ConfigManager;
+import me.okunxw.cfreward.utils.PromoManager;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CFReward extends JavaPlugin {
+    private static CFReward instance;
+    private static Economy econ = null;
+    private PromoManager promoManager;
+    private ConfigManager configManager;
 
     @Override
     public void onEnable() {
-        getLogger().info("CFReward загружен!");
-
-        // Проверка наличия Vault
-        if (Bukkit.getPluginManager().getPlugin("Vault") == null) {
-            getLogger().warning("Vault не найден! Экономика работать не будет.");
-        } else {
-            getLogger().info("Vault найден, подключаемся...");
-            // Инициализация экономики через Vault
-            setupEconomy();
+        instance = this;
+        saveDefaultConfig();
+        configManager = new ConfigManager(this);
+        promoManager = new PromoManager(this);
+        
+        if (!setupEconomy()) {
+            getLogger().warning("[CFReward] Vault не найден! Экономическая система отключена.");
         }
-
-        // Проверка наличия LuckPerms
-        if (Bukkit.getPluginManager().getPlugin("LuckPerms") == null) {
-            getLogger().warning("LuckPerms не найден! Выдача прав не будет работать.");
-        } else {
-            getLogger().info("LuckPerms найден, подключаемся...");
-            // Инициализация системы прав через LuckPerms
-            setupPermissions();
-        }
-
-        // Регистрация команд
-        getCommand("cfreward").setExecutor(this);
+        
+        registerCommands();
+        registerEvents();
+        getLogger().info("[CFReward] Плагин успешно загружен!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("CFReward отключен.");
+        getLogger().info("[CFReward] Плагин отключен.");
     }
 
-    // Метод для инициализации экономики
-    private void setupEconomy() {
-        // Код для настройки экономики через Vault
+    private void registerCommands() {
+        getCommand("promo").setExecutor((CommandExecutor) new PromoCommand(this));
     }
 
-    // Метод для инициализации системы прав
-    private void setupPermissions() {
-        // Код для настройки прав через LuckPerms
+    private void registerEvents() {
+        PluginManager pm = Bukkit.getPluginManager();
+        // pm.registerEvents(new ExampleListener(this), this); // Если понадобятся события
     }
 
-    // Обработка команд
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (cmd.getName().equalsIgnoreCase("cfreward")) {
-            if (args.length == 0) {
-                sender.sendMessage("Использование: /cfreward [args]");
-                return true;
-            }
-            // Обработка подкоманд
-            if (args[0].equalsIgnoreCase("help")) {
-                sender.sendMessage("Список доступных команд:");
-                // Добавь сюда список команд
-            } else if (args[0].equalsIgnoreCase("create")) {
-                if (sender instanceof Player) {
-                    Player player = (Player) sender;
-                    // Код для создания промокода
-                    player.sendMessage("Промокод создан!");
-                } else {
-                    sender.sendMessage("Эту команду может выполнить только игрок.");
-                }
-            }
-            // Добавь обработку других подкоманд
-            return true;
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
         }
-        return false;
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+    
+    public static CFReward getInstance() {
+        return instance;
+    }
+    
+    public PromoManager getPromoManager() {
+        return promoManager;
+    }
+    
+    public ConfigManager getConfigManager() {
+        return configManager;
+    }
+    
+    public static Economy getEconomy() {
+        return econ;
     }
 }
